7e660d1f32d591618377c5f72d01bb9e5995a5dd
MUSTAQ1995 <mahmadmustaq1920@gmail.com>
2023-12-11 16:02:14 +0530

    Setting up the project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+// NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_cmVndWxhci1zdW5iZWFtLTU3LmNsZXJrLmFjY291bnRzLmRldiQ
+// CLERK_SECRET_KEY=sk_test_lvRPctLsyKP2y4RfzuD0Bks8rvEOSWfG6wUTLa4TmY
+export default Dashboard
